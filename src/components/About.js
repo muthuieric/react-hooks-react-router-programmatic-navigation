a81caa0a201fd0f280ff227d163ddb9634045128
Eric <ericmuthuipatch22@gmail.com>
2023-07-28 10:50:39 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -5,3 +5,27 @@ function About() {
 }
 
 export default About;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function About() {
+//   return <h1>This is my about component!</h1>;
+// }
+
+// export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,3 +12,30 @@ function Home({ isLoggedIn }) {
 }
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { Redirect } from "react-router-dom";
+
+// function Home({ isLoggedIn }) {
+//   if (!isLoggedIn) return <Redirect to="/login" />;
+
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
+
+// export default Home;
